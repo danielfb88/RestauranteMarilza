af74472f3fc0583a141222facaa1cfa4a9622cfd
Daniel Bonfim <daniel.fb88@gmail.com>
2018-10-29 12:31:52 -0300

    Prato repository

---
+++ src/main/java/com/dboffice/restaurantedamarilza/api/repositories/PratoRepository.java
@@ -0,0 +1,12 @@
+package com.dboffice.restaurantedamarilza.api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dboffice.restaurantedamarilza.api.entities.PratoEntity;
+
+public interface PratoRepository extends JpaRepository<PratoEntity, Long> {
+	
+	@Transactional(readOnly = true)
+	PratoRepository findByDescricao(String descricao);
+}
