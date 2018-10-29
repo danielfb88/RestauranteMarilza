71a09fc85e18a0ffe9af8d196b995127e146fb3a
Daniel Bonfim <daniel.fb88@gmail.com>
2018-10-29 11:29:36 -0300

    Primeiro commit

---
+++ src/main/java/com/dboffice/restaurantedamarilza/api/RestauranteDaMarilzaApplication.java
@@ -0,0 +1,12 @@
+package com.dboffice.restaurantedamarilza.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteDaMarilzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteDaMarilzaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dboffice/restaurantedamarilza/api/RestauranteDaMarilzaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dboffice.restaurantedamarilza.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestauranteDaMarilzaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
